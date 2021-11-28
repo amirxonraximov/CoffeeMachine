12099ec3c58faa97d2101b6a810207e2462a993a
Amirxon <arm19980201@gmail.com>
2021-11-28 11:58:50 +0500

    fix spelling

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -9,7 +9,7 @@ public class CoffeeMachine {
         int n = g.nextInt();
         System.out.println("For " + n + " cups of coffee you will need:");
         System.out.println(n * 200 + " ml of water");
-        System.out.println(n * 50 + " mlof milk");
+        System.out.println(n * 50 + " ml of milk");
         System.out.println(n * 15 + " g of coffee beans");
     }
 }
